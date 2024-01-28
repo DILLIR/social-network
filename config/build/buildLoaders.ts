7f075f661114636d3476e3d6b7d1495831266635
Yurii Soma <yurii.soma@student.tuke.sk>
2024-01-28 14:51:14 +0100

    add config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,31 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (path: string) => /\.module\.scss$/.test(path),
+            localIdentName: options.isDev
+              ? "[path][name]__[local]"
+              : "hash:base64:8",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
+  return [typescriptLoader, scssLoader];
+}
