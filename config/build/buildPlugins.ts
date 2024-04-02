1594c6705e5a903e7dbccdc054228dba64a8ecdd
Yurii Soma <yurii.soma@student.tuke.sk>
2024-04-02 19:58:06 +0200

    update buildPlugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -20,12 +20,12 @@ export function buildPlugins({
         }),
         new webpack.DefinePlugin({
             __IS__DEV__: JSON.stringify(isDev)
-        }),
-        new BundleAnalyzerPlugin({ openAnalyzer: false })
+        })
     ];
 
     if (isDev) {
         plugins.push(new ReactRefreshWebpackPlugin());
+        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
     }
 
     return plugins;
