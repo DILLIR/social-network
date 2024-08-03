9ae22e31ab41ec6f2a1f1fd6118a77873489d6f6
Yurii Soma <yurii.soma@student.tuke.sk>
2024-08-03 20:31:58 +0200

    Add new theme and npm concurrent

--- src/app/providers/ThemeProvider/lib/useTheme.tsx
+++ src/app/providers/ThemeProvider/lib/useTheme.tsx
@@ -1,16 +1,29 @@
-import { LOCAL_STORAGE_KEY, Theme, ThemeContext } from "../lib/ThemeContext";
-import { useContext } from "react";
+import { useContext } from 'react';
+import { LOCAL_STORAGE_KEY, Theme, ThemeContext } from '../lib/ThemeContext';
 
 interface useThemeResult {
-  theme: Theme;
-  toggleTheme: () => void;
+    theme: Theme;
+    toggleTheme: () => void;
 }
 
 export function useTheme(): useThemeResult {
     const { theme, setTheme } = useContext(ThemeContext);
 
     const toggleTheme = () => {
-        const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+        let newTheme: Theme;
+        switch (theme) {
+        case Theme.LIGHT:
+            newTheme = Theme.DARK;
+            break;
+        case Theme.DARK:
+            newTheme = Theme.GREEN;
+            break;
+        case Theme.GREEN:
+            newTheme = Theme.LIGHT;
+            break;
+        default:
+            newTheme = Theme.LIGHT;
+        }
         setTheme?.(newTheme);
         document.body.className = newTheme;
         localStorage.setItem(LOCAL_STORAGE_KEY, newTheme);
@@ -20,6 +33,6 @@ export function useTheme(): useThemeResult {
 
     return {
         theme: theme || Theme.LIGHT,
-        toggleTheme,
+        toggleTheme
     };
 }
