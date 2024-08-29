28f712a388a689d91feb9df06be957a1e879211e
Yurii Soma <yurii.soma@student.tuke.sk>
2024-08-29 21:32:13 +0200

    Production

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import webpack, { WebpackPluginInstance } from 'webpack';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BuildOptions } from './types/config';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import CopyPlugin from 'copy-webpack-plugin';
 
 export function buildPlugins({
     paths,
@@ -24,12 +25,20 @@ export function buildPlugins({
             __IS_DEV__: JSON.stringify(isDev),
             __API_URL__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project)
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: paths.locales,
+                    to: paths.buildLocales
+                }
+            ]
         })
     ];
 
     if (isDev) {
         plugins.push(new ReactRefreshWebpackPlugin());
-        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }))
+        plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
     }
 
     return plugins;
