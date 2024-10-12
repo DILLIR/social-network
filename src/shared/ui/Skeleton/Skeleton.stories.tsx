a5ad379bcd68088b3d364db1f7f941f554870e38
Yurii Soma <yurii.soma@student.tuke.sk>
2024-10-12 15:18:11 +0200

    Remove theme stories form Skeleton

--- src/shared/ui/Skeleton/Skeleton.stories.tsx
+++ src/shared/ui/Skeleton/Skeleton.stories.tsx
@@ -1,6 +1,4 @@
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
-import { Theme } from '@/shared/const/theme';
-import { ThemeDecorator } from '../../config/storybook/ThemeDecorator/ThemeDecorator';
 import { Skeleton } from './Skeleton';
 import type { Meta, StoryObj } from '@storybook/react';
 
@@ -27,38 +25,4 @@ export const Circle: Story = {
         height: 100,
         width: 100,
     },
-};
-
-export const DefaultDark: Story = {
-    args: {
-        width: '100%',
-        height: '200px',
-    },
-    decorators: [ThemeDecorator(Theme.DARK)],
-};
-
-export const CircleDark: Story = {
-    args: {
-        border: '50%',
-        height: 100,
-        width: 100,
-    },
-    decorators: [ThemeDecorator(Theme.DARK)],
-};
-
-export const DefaultGreen: Story = {
-    args: {
-        width: '100%',
-        height: '200px',
-    },
-    decorators: [ThemeDecorator(Theme.GREEN)],
-};
-
-export const CircleGreen: Story = {
-    args: {
-        border: '50%',
-        height: 100,
-        width: 100,
-    },
-    decorators: [ThemeDecorator(Theme.GREEN)],
 };
