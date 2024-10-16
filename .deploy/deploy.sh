44d87cad5702c6396b45319c7a3170d54cfaf44c
Yurii Soma <yurii.soma@student.tuke.sk>
2024-10-16 05:11:40 +0200

    Update deploy script

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -1,5 +1,5 @@
-cd ..
+cd /root/social-network
 npm run build:prod
 
 rm -rf /root/../var/www/social-network/html
-mv /root/.delpoy/build ~/../var/www/social-network/html
+mv /root/social-network/build ~/../var/www/social-network/html
